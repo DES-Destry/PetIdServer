c80e09e3aa70c1294da56361c626fe611c122fc7
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-09-06 16:54:57 +0600

    feat: create pet method

---
+++ PetIdServer.Application/Dto/VoidResponseDto.cs
@@ -0,0 +1,6 @@
+namespace PetIdServer.Application.Dto;
+
+public class VoidResponseDto
+{
+    public bool Executed { get; set; }
+}
---
+++ PetIdServer.Application/Requests/Commands/Pet/Create/CreatePetCommandHandler.cs
@@ -0,0 +1,25 @@
+using MediatR;
+using PetIdServer.Application.Dto;
+using PetIdServer.Application.Repositories;
+
+namespace PetIdServer.Application.Requests.Commands.Pet.Create;
+
+public class CreatePetCommandHandler : IRequestHandler<CreatePetCommand, VoidResponseDto>
+{
+    private readonly IPetRepository _petRepository;
+    
+    public async Task<VoidResponseDto> Handle(CreatePetCommand request, CancellationToken cancellationToken)
+    {
+        var creationAttributes = new Core.Entities.Pet.CreationAttributes(
+            Guid.NewGuid(),
+            request.Type,
+            request.Name,
+            request.Sex,
+            request.IsCastrated);
+        var pet = new Core.Entities.Pet(creationAttributes);
+
+        await _petRepository.CreatePet(pet);
+
+        return new VoidResponseDto() { Executed = true };
+    }
+}
