af3e8526db39db1269089d89e321f61aeb659efc
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-09-11 17:06:01 +0600

    feat: enrich db context

--- PetIdServer.Infrastructure/Database/PetIdContext.cs
+++ PetIdServer.Infrastructure/Database/PetIdContext.cs
@@ -1,16 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using PetIdServer.Infrastructure.Database.Models;
 
 namespace PetIdServer.Infrastructure.Database;
 
 public class PetIdContext : DbContext
 {
+    public DbSet<OwnerModel> Owners { get; set; }
+    public DbSet<OwnerContactModel> OwnerContacts { get; set; }
+    public DbSet<PetModel> Pets { get; set; }
+    public DbSet<TagModel> Tags { get; set; }
+    
     public PetIdContext(DbContextOptions<PetIdContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("pet");
+
+        modelBuilder.Entity<OwnerModel>().Navigation(owner => owner.Pets).AutoInclude();
+        modelBuilder.Entity<OwnerModel>().Navigation(owner => owner.Contacts).AutoInclude();
         
-        // Default Guid Id value
+        modelBuilder.Entity<TagModel>().Navigation(owner => owner.Pet).AutoInclude();
         
         base.OnModelCreating(modelBuilder);
     }
