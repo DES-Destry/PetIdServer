deca3e177694b251f25ef5729e4b00a8a5d335ac
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-09-26 02:54:21 +0600

    fix: composite keys for owner contact

--- PetIdServer.Infrastructure/Database/Models/OwnerContactModel.cs
+++ PetIdServer.Infrastructure/Database/Models/OwnerContactModel.cs
@@ -1,9 +1,10 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace PetIdServer.Infrastructure.Database.Models;
 
-[Table("owners_contacts")]
+[Table("owners_contacts"), PrimaryKey(nameof(OwnerId), nameof(ContactType))]
 public class OwnerContactModel
 {
     [Column("owner_id"), Required, Key] public string OwnerId { get; set; }
@@ -12,7 +13,7 @@ public class OwnerContactModel
     public string ContactType { get; set; }
 
     [Column("contact"), Required] public string Contact { get; set; }
-    
-    
+
+
     [ForeignKey("OwnerId")] public virtual OwnerModel Owner { get; set; }
 }
