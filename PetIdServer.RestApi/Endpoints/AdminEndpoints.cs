de486b9a961e9f5ec9f49fd0d69832a88bd7c71a
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-11-23 18:34:40 +0600

    feat: expose get decoded tag to api methods

--- PetIdServer.RestApi/Endpoints/AdminEndpoints.cs
+++ PetIdServer.RestApi/Endpoints/AdminEndpoints.cs
@@ -3,6 +3,7 @@ using Carter;
 using MediatR;
 using PetIdServer.Application.Requests.Commands.Admin.ChangePassword;
 using PetIdServer.Application.Requests.Commands.Admin.Login;
+using PetIdServer.Application.Requests.Queries.Tag.GetDecoded;
 using PetIdServer.RestApi.Auth;
 using PetIdServer.RestApi.Binding;
 using PetIdServer.RestApi.Endpoints.Dto.Admin;
@@ -41,6 +42,15 @@ public class AdminEndpoints : ICarterModule
             op.Summary = "Change password as authenticated admin.";
             return op;
         });
+
+        group.MapGet("tag/{id:int}", GetDecodedTag)
+            .RequireAuthorization(AuthSchemas.Admin)
+            .WithName(nameof(GetDecodedTag))
+            .WithOpenApi(op =>
+            {
+                op.Summary = "Get tag with his public code with admin credentials.";
+                return op;
+            });;
     }
     
     private static async Task<IResult> Authenticate(RequestAdmin admin, ISender sender, IMapper mapper)
@@ -61,6 +71,14 @@ public class AdminEndpoints : ICarterModule
         var command = new ChangePasswordCommand
             {Id = admin.Username, OldPassword = dto.OldPassword, NewPassword = dto.NewPassword};
         var response = await sender.Send(command);
+
+        return Results.Ok(response);
+    }
+
+    private static async Task<IResult> GetDecodedTag(int id, ISender sender)
+    {
+        var query = new GetDecodedTagQuery {Id = id};
+        var response = await sender.Send(query);
 
         return Results.Ok(response);
     }
