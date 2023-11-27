d564808af30dbb7ce11de232d8617c5fa7191922
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-11-27 13:33:25 +0600

    fix: tag not found error code

--- PetIdServer.Core/Exceptions/Tag/TagNotFoundException.cs
+++ PetIdServer.Core/Exceptions/Tag/TagNotFoundException.cs
@@ -2,10 +2,18 @@ namespace PetIdServer.Core.Exceptions.Tag;
 
 public class TagNotFoundException : CoreException
 {
-    public override string Code { get; protected set; } = CoreExceptionCode.TagAlreadyInUse;
+    public TagNotFoundException(string message = "Tag not found") : base(message)
+    {
+    }
+
+    public TagNotFoundException(object metadata) : base(metadata)
+    {
+    }
+
+    public TagNotFoundException(string message, object metadata) : base(message, metadata)
+    {
+    }
+
+    public override string Code { get; protected set; } = CoreExceptionCode.TagNotFound;
     public override CoreExceptionKind? Kind => CoreExceptionKind.EntityNotFound;
-    
-    public TagNotFoundException(string message = "Tag not found") : base(message) { }
-    public TagNotFoundException(object metadata) : base(metadata) { }
-    public TagNotFoundException(string message, object metadata): base(message, metadata) { }
 }
