cd879a5fea81f1014f669a4ec00358769a54eaf9
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-11-27 14:50:15 +0600

    fix: create tags dto parsing

--- PetIdServer.RestApi/Endpoints/Dto/Admin/CreateTagsDto.cs
+++ PetIdServer.RestApi/Endpoints/Dto/Admin/CreateTagsDto.cs
@@ -2,7 +2,7 @@ namespace PetIdServer.RestApi.Endpoints.Dto.Admin;
 
 public class CreateTagsDto
 {
-    public int FromId { get; set; }
-    public int ToId { get; set; }
+    public int IdFrom { get; set; }
+    public int IdTo { get; set; }
     public IEnumerable<string> Codes { get; set; }
 }
