ce392f984fdbc85e68074610742efcd2878c9499
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-11-27 13:32:32 +0600

    fix: code decoder private key reading

--- PetIdServer.Infrastructure/Services/CodeDecoder.cs
+++ PetIdServer.Infrastructure/Services/CodeDecoder.cs
@@ -1,22 +1,21 @@
 using System.Text;
-using PetIdServer.Application.Services;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.OpenSsl;
+using PetIdServer.Application.Services;
 
 namespace PetIdServer.Infrastructure.Services;
 
 public class CodeDecoder : ICodeDecoder
 {
-    private enum Action { Decrypt, Encrypt }
-    
-    private readonly AsymmetricKeyParameter _publicCrt;
     private readonly AsymmetricKeyParameter _privateCrt;
+
+    private readonly AsymmetricKeyParameter _publicCrt;
 
     public CodeDecoder()
     {
-        var privateKey = File.ReadAllText("../Keys/private_key.pem");
-        
+        var privateKey = File.ReadAllText("./Keys/private_key.pem");
+
         var pemReader = new PemReader(new StringReader(privateKey));
         var keyPair = pemReader.ReadObject() as AsymmetricCipherKeyPair;
 
@@ -26,14 +25,21 @@ public class CodeDecoder : ICodeDecoder
                       throw new ArgumentException("Cannot create private key crt parameters", nameof(privateKey));
     }
 
-    public async Task<string> EncodePublicCode(string publicCode) => await Execute(Action.Encrypt, publicCode);
-    public async Task<string> GetPublicCodeOriginal(string privateCode) => await Execute(Action.Decrypt, privateCode);
+    public async Task<string> EncodePublicCode(string publicCode)
+    {
+        return await Execute(Action.Encrypt, publicCode);
+    }
+
+    public async Task<string> GetPublicCodeOriginal(string privateCode)
+    {
+        return await Execute(Action.Decrypt, privateCode);
+    }
 
     private async Task<string> Execute(Action action, string code)
     {
         var forEncryption = action == Action.Encrypt;
         var crt = forEncryption ? _publicCrt : _privateCrt;
-        
+
         var rsaEngine = new RsaEngine();
         rsaEngine.Init(forEncryption, crt);
 
@@ -45,5 +51,11 @@ public class CodeDecoder : ICodeDecoder
         var decrypted = Encoding.UTF8.GetString(decryptedBytes);
 
         return await Task.FromResult(decrypted);
+    }
+
+    private enum Action
+    {
+        Decrypt,
+        Encrypt
     }
 }
