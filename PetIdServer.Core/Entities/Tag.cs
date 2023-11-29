a04d2761f3178a6f460a4aee9c4f119784b3b632
Andrey Kirik <dev.andrey.kirik@gmail.com>
2023-11-29 14:35:06 +0600

    feat: add new fields for tag with date-times

--- PetIdServer.Core/Entities/Tag.cs
+++ PetIdServer.Core/Entities/Tag.cs
@@ -6,26 +6,31 @@ namespace PetIdServer.Core.Entities;
 
 public class Tag : Entity<TagId>
 {
-    public record CreationAttributes(TagId Id, string Code, long ControlCode);
-
-    /// <summary>
-    /// A private code
-    /// </summary>
-    public string Code { get; set; } = string.Empty;
-    public long ControlCode { get; set; }
-    public Pet? Pet { get; private set; }
-
-    public bool IsAlreadyInUse => Pet is not null;
-
     public Tag(CreationAttributes creationAttributes) : base(creationAttributes.Id)
     {
         Code = creationAttributes.Code;
         ControlCode = creationAttributes.ControlCode;
     }
-    
+
     public Tag(TagId id) : base(id)
     {
     }
+
+    /// <summary>
+    ///     A private code
+    /// </summary>
+    public string Code { get; set; } = string.Empty;
+
+    public long ControlCode { get; set; }
+    public Pet? Pet { get; private set; }
+
+    public bool IsAlreadyInUse => Pet is not null;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? PetAddedAt { get; set; }
+
+    public DateTime? LastScannedAt { get; set; }
 
     public void SetupPet(Pet pet)
     {
@@ -33,4 +38,6 @@ public class Tag : Entity<TagId>
 
         Pet = pet;
     }
+
+    public record CreationAttributes(TagId Id, string Code, long ControlCode);
 }
--- PetIdServer.Infrastructure/Database/Models/TagModel.cs
+++ PetIdServer.Infrastructure/Database/Models/TagModel.cs
@@ -6,18 +6,20 @@ namespace PetIdServer.Infrastructure.Database.Models;
 [Table("tags")]
 public class TagModel
 {
-    [Column("id"), Required, Key]
-    public int Id { get; set; }
+    [Column("id")] [Required] [Key] public int Id { get; set; }
 
-    [Column("code"), Required]
-    public string Code { get; set; }
-    
-    [Column("control_code"), Required]
-    public long ControlCode { get; set; }
+    [Column("code")] [Required] public string Code { get; set; }
 
-    [Column("pet_id")]
-    public Guid? PetId { get; set; }
-    
-    
+    [Column("control_code")] [Required] public long ControlCode { get; set; }
+
+    [Column("pet_id")] public Guid? PetId { get; set; }
+
+    [Column("created_at")] public DateTime CreatedAt { get; set; }
+
+    [Column("pet_added_at")] public DateTime PetAddedAt { get; set; }
+
+    [Column("last_scanned_at")] public DateTime LastScannedAt { get; set; }
+
+
     [ForeignKey("PetId")] public virtual PetModel? Pet { get; set; }
 }
