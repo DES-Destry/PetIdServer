5beab8463910d4b0dd6ba7b3f94e5f3c6df69a12
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-06-08 02:07:30 +0500

    fix: add port for postgres

--- PetIdServer.AppHost/Program.cs
+++ PetIdServer.AppHost/Program.cs
@@ -5,18 +5,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 var pgPassword = builder.AddParameter("pgPassword", true);
 
 var db = builder
-    .AddPostgres("pg", password: pgPassword)
+    .AddPostgres("pg", port: 5432, password: pgPassword)
     .WithDataVolume("petidserver_pet-id_pgdata")
     .AddDatabase("pet-id");
-
-
-// var redis = builder
-//     .AddRedis("redis")
-//     .WithDataVolume("petidserver_pet-id_redis-data");
 
 builder
     .AddProject<PetIdServer_RestApi>("api")
     .WithReference(db);
-// .WithReference(redis);
 
 builder.Build().Run();
