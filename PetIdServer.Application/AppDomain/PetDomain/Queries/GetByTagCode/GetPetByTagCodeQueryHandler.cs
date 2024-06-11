0fb500c1c48c99f08f382dc920186fe2718b85da
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-06-11 06:48:55 +0500

    fix: tag fetching

--- PetIdServer.Application/AppDomain/PetDomain/Queries/GetByTagCode/GetPetByTagCodeQueryHandler.cs
+++ PetIdServer.Application/AppDomain/PetDomain/Queries/GetByTagCode/GetPetByTagCodeQueryHandler.cs
@@ -1,20 +1,21 @@
-using AutoMapper;
 using MediatR;
 using PetIdServer.Application.AppDomain.TagDomain;
+using PetIdServer.Application.Common.Services;
 using PetIdServer.Application.Common.Services.Dto;
 using PetIdServer.Core.Domain.Pet.Exceptions;
 using PetIdServer.Core.Domain.Tag.Exceptions;
 
 namespace PetIdServer.Application.AppDomain.PetDomain.Queries.GetByTagCode;
 
-public class GetPetByTagCodeQueryHandler(ITagRepository tagRepository, IMapper mapper)
+public class GetPetByTagCodeQueryHandler(ITagRepository tagRepository, ICodeDecoder codeDecoder)
     : IRequestHandler<GetPetByTagCodeQuery, PetDto>
 {
     public async Task<PetDto> Handle(
         GetPetByTagCodeQuery request,
         CancellationToken cancellationToken)
     {
-        var tag = await tagRepository.GetByCode(request.Code) ??
+        var privateCode = await codeDecoder.GetPublicCodeOriginal(request.Code);
+        var tag = await tagRepository.GetByCode(privateCode) ??
                   throw new TagNotFoundException("Tag with this code doesn't exists", new
                   {
                       Query = nameof(GetPetByTagCodeQuery), request.Code
--- PetIdServer.Application/AppDomain/TagDomain/Queries/GetByCode/GetTagByCodeQueryHandler.cs
+++ PetIdServer.Application/AppDomain/TagDomain/Queries/GetByCode/GetTagByCodeQueryHandler.cs
@@ -1,18 +1,20 @@
 using MediatR;
 using PetIdServer.Application.AppDomain.PetDomain.Dto;
 using PetIdServer.Application.AppDomain.TagDomain.Dto;
+using PetIdServer.Application.Common.Services;
 using PetIdServer.Core.Domain.Tag.Exceptions;
 
 namespace PetIdServer.Application.AppDomain.TagDomain.Queries.GetByCode;
 
-public class GetTagByCodeQueryHandler(ITagRepository tagRepository)
+public class GetTagByCodeQueryHandler(ITagRepository tagRepository, ICodeDecoder codeDecoder)
     : IRequestHandler<GetTagByCodeQuery, CheckTagDto>
 {
     public async Task<CheckTagDto> Handle(
         GetTagByCodeQuery request,
         CancellationToken cancellationToken)
     {
-        var tag = await tagRepository.GetByCode(request.Code) ??
+        var privateCode = await codeDecoder.GetPublicCodeOriginal(request.Code);
+        var tag = await tagRepository.GetByCode(privateCode) ??
                   throw new TagNotFoundException("Tag with this code doesn't exists", new
                   {
                       Query = nameof(GetTagByCodeQuery), request.Code
