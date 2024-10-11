31273d02fead840775afe324e6b78bb849acadff
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-10-12 01:59:02 +0500

    fix: tests
    This test is impossible. Architecture should be changed

--- PetIdServer.Infrastructure/Services/CodeDecoder.cs
+++ PetIdServer.Infrastructure/Services/CodeDecoder.cs
@@ -6,45 +6,79 @@ namespace PetIdServer.Infrastructure.Services;
 
 public class CodeDecoder : ICodeDecoder
 {
-    private readonly RSAParameters _rsaParameters;
+    private readonly string _privateKey = File.ReadAllText(Path.Combine(
+        AppDomain.CurrentDomain.BaseDirectory + "../../../", "Keys", "private.pem"));
+    // private readonly RSAParameters _privateRsaParameters;
 
-    public CodeDecoder()
+    private readonly string _publicKey = File.ReadAllText(Path.Combine(
+        AppDomain.CurrentDomain.BaseDirectory + "../../../", "Keys", "public.pem"));
+    // private readonly RSAParameters _publicRsaParameters;
+
+    // _publicRsaParameters = ExtractPublicRsaParameters(_publicKey);
+    // _privateRsaParameters = ExtractPrivateRsaParameters(_privateKey);
+
+    public async Task<string> EncodePublicCode(string publicCode)
     {
-        var privateKey = File.ReadAllText(Path.Combine(
-            AppDomain.CurrentDomain.BaseDirectory + "../../../", "Keys", "private.pem"));
-        _rsaParameters = ExtractRsaParameters(privateKey);
+        var publicBytes = Encoding.UTF8.GetBytes(publicCode);
+
+        using var rsa = RSA.Create();
+        rsa.ImportFromPem(_publicKey);
+
+        var encryptedBytes = rsa.Encrypt(publicBytes, RSAEncryptionPadding.Pkcs1);
+        return await Task.FromResult(Convert.ToBase64String(encryptedBytes));
     }
 
-    public async Task<string> EncodePublicCode(string publicCode) =>
-        await Execute(Action.Encrypt, publicCode);
-
-    public async Task<string> GetPublicCodeOriginal(string privateCode) =>
-        await Execute(Action.Decrypt, privateCode);
-
-    private async Task<string> Execute(Action action, string code)
+    public async Task<string> GetPublicCodeOriginal(string privateCode)
     {
-        using var rsaProvider = RSA.Create();
-        rsaProvider.ImportParameters(_rsaParameters);
+        var privateBytes = Convert.FromBase64String(privateCode);
 
-        var inputCodeBytes = Convert.FromBase64String(code);
+        using var rsa = RSA.Create();
+        rsa.ImportFromPem(_privateKey);
 
-        var resultCodeBytes = action switch
-        {
-            Action.Decrypt => rsaProvider.Decrypt(inputCodeBytes, RSAEncryptionPadding.OaepSHA256),
-            Action.Encrypt => rsaProvider.Encrypt(inputCodeBytes, RSAEncryptionPadding.OaepSHA256),
-            _ => []
-        };
-
-        var resultCode = Encoding.UTF8.GetString(resultCodeBytes);
-        return await Task.FromResult(resultCode);
+        var decryptedBytes = rsa.Decrypt(privateBytes, RSAEncryptionPadding.Pkcs1);
+        return await Task.FromResult(Encoding.UTF8.GetString(decryptedBytes));
     }
 
-    private static RSAParameters ExtractRsaParameters(string privateKey)
-    {
-        var rsa = new RSACryptoServiceProvider();
-        rsa.ImportFromPem(privateKey);
-        return rsa.ExportParameters(true);
-    }
+    // private async Task<string> Execute(Action action, string code)
+    // {
+    //     using var publicProvider = RSA.Create();
+    //     using var privateProvider = RSA.Create();
+    //
+    //     publicProvider.ImportParameters(_publicRsaParameters);
+    //     privateProvider.ImportParameters(_privateRsaParameters);
+    //
+    //     var resultCodeBytes = action switch
+    //     {
+    //         Action.Decrypt => privateProvider.Decrypt(Convert.FromBase64String(code),
+    //             RSAEncryptionPadding.OaepSHA256),
+    //         Action.Encrypt => privateProvider.Encrypt(Convert.FromHexString(code),
+    //             RSAEncryptionPadding.OaepSHA256),
+    //         _ => []
+    //     };
+    //
+    //     var resultCode = action switch
+    //     {
+    //         Action.Decrypt => Encoding.UTF8.GetString(resultCodeBytes),
+    //         Action.Encrypt => Convert.ToBase64String(resultCodeBytes),
+    //         _ => string.Empty
+    //     };
+    //
+    //     return await Task.FromResult(resultCode);
+    // }
 
-    private enum Action { Decrypt, Encrypt }
+    // private static RSAParameters ExtractPublicRsaParameters(string publicKey)
+    // {
+    //     RSACryptoServiceProvider rsa = new();
+    //     rsa.ImportFromPem(publicKey);
+    //     return rsa.ExportParameters(false);
+    // }
+    //
+    // private static RSAParameters ExtractPrivateRsaParameters(string privateKey)
+    // {
+    //     RSACryptoServiceProvider rsa = new();
+    //     rsa.ImportFromPem(privateKey);
+    //     return rsa.ExportParameters(true);
+    // }
+    //
+    // private enum Action { Decrypt, Encrypt }
 }
