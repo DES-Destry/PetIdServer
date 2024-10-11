8c450ea755a5686c124a1622a84e41aba02f7ff8
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-10-11 20:46:41 +0500

    test: unit tests for code-decoder

---
+++ PetIdServer.Tests/CodeDecoderTests.cs
@@ -0,0 +1,22 @@
+﻿using PetIdServer.Application.Common.Services;
+using PetIdServer.Tests.Extensions;
+
+namespace PetIdServer.Tests;
+
+public class CodeDecoderTests
+{
+    private const string PublicCodeExample1 = "123456789";
+
+    private readonly ICodeDecoder _codeDecoder =
+        TestDependencies.GetRequiredService<ICodeDecoder>() ??
+        throw new NullReferenceException(nameof(_codeDecoder));
+
+    [Test]
+    public async Task EncodingAndDecodingEndsWithInitialValue()
+    {
+        var encoded = await _codeDecoder.EncodePublicCode(PublicCodeExample1);
+        var decoded = await _codeDecoder.GetPublicCodeOriginal(encoded);
+
+        await Assert.That(decoded).IsEqualTo(PublicCodeExample1);
+    }
+}
