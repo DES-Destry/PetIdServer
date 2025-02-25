5427544a44d15108d86e45e9a3621f11a0f8f789
Andrey Kirik <dev.andrey.kirik@gmail.com>
2025-02-25 09:37:53 +0500

    feat: accept tag features in rest api

--- PetIdServer.RestApi/Endpoints/Dto/Admin/CreateTagsDto.cs
+++ PetIdServer.RestApi/Endpoints/Dto/Admin/CreateTagsDto.cs
@@ -2,12 +2,10 @@ using PetIdServer.Application.Tags.Commands.CreateBatch;
 
 namespace PetIdServer.RestApi.Endpoints.Dto.Admin;
 
-public record CreateTagsDto(int IdFrom, int IdTo, IEnumerable<string> Codes)
+public record CreateTagsDto(int IdFrom, int IdTo, IEnumerable<string> Codes, IEnumerable<string>? Features)
 {
     public CreateTagsBatchCommand ToCommand() => new()
     {
-        IdFrom = IdFrom,
-        IdTo = IdTo,
-        Codes = Codes
+        IdFrom = IdFrom, IdTo = IdTo, Codes = Codes, Features = Features
     };
 }
