04cb214d5dd05b9cc2fc79bb5f580d509f2563a1
Andrey Kirik <dev.andrey.kirik@gmail.com>
2025-02-26 14:13:41 +0500

    feat: add permissions for jwt tokens

---
+++ PetIdServer.Infrastructure/Services/PetIdClaimTypes.cs
@@ -0,0 +1,9 @@
+namespace PetIdServer.Infrastructure.Services;
+
+public static class PetIdClaimTypes
+{
+    public const string UserJson = "UserJson";
+    public const string WithPermissionsOf = "WithPermissionsOf";
+
+    public const string Token = "Token";
+}
